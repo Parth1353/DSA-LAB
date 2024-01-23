614abb2d47b48819d9f88c1e9cd91abca1d5a5d1
Parth Saini <parthsaini13@gmail.com>
2024-01-23 10:38:01 +0530

    fixed

---
+++ 3rd Program/sampel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int sort(int arr[], int size)
+{
+    for (int i = 0; i < size; i++) 
+    {
+        
+    }}
+        
+int main() {
+    
+    return 0;
+}
